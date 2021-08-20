fcc0cb1bd2daebb95db299874be272544e95d811
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-20 20:34:26 +0200

    [update] from css file to styled components

--- src/components/TypeCard.js
+++ src/components/TypeCard.js
@@ -1,12 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import "../static/css/PokeTypes.css";
 
 const TypeCard = (props) => {
   return (
-    <div className="typeCard" id={props.type.name}>
+    <PokeTypeCard id={props.type.name}>
       <p> {props.type.name}</p>
-    </div>
+    </PokeTypeCard>
   );
 };
+
+const PokeTypeCard = styled.div`
+  margin: 20px;
+  padding: 20px;
+  text-align: center;
+  color: black;
+  font-size: 20px;
+  background-color: rgba(0, 0, 0, 0.4);
+  border: 1px solid black;
+  border-radius: 20px;
+  width: 100px;
+  height: 100px;
+`;
 
 export default TypeCard;
