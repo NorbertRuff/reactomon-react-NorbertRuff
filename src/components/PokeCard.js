504ccdf6c95e5afb18edca97f5832ded5eb41bde
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-20 15:39:11 +0200

    [add] Hooks useeffect and data objects

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const PokeCard = (props) => {
-  // const pokeId = props.pokemon.url.split("/").slice(-2).slice(0, -1);
-  const [pokeId, setPokeId] = useState();
+  const [pokemon, setPokemon] = useState({
+    id: null,
+    name: null,
+    frontpic: null,
+    backpic: null,
+    dreamWorld: null,
+    types: [],
+  });
 
   useEffect(() => {
-    setPokeId(props.pokemon.url.split("/").slice(-2).slice(0, -1));
+    axios.get(props.pokemon.url).then((res) => {
+      console.log(res.data.sprites);
+      setPokemon({
+        id: res.data.id,
+        name: res.data.name,
+        frontpic: res.data.sprites.front_default,
+        backpic: res.data.sprites.back_default,
+        dreamWorld: res.data.sprites.other.dream_world.front_default,
+        types: res.data.types,
+      });
+    });
   }, [props.pokemon.url]);
 
   return (
     <div className="PokeCard" id={props.pokemon.name}>
-      <Link key={props.pokemon.name} to={`/pokemons/${pokeId}`}>
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/imgbot/sprites/pokemon/${pokeId}.png`}
-          alt="pokemon"
-        />
+      <Link key={props.pokemon.name} to={`/pokemons/${pokemon.id}`}>
+        <img src={pokemon.dreamWorld} alt="pokemon" />
         <p> {props.pokemon.name}</p>
+
+        <div>
+          Type:
+          {pokemon.types.map((type) => (
+            <div>
+              <ul>
+                <li>{type.type.name}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
       </Link>
     </div>
   );
