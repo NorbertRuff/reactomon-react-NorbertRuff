539448a0a6dfa86cf9dbcd26f74bc6c92a4e7338
NorbertRuff <ruffnorbert88@gmail.com>
2021-08-21 17:18:25 +0200

    [add] next and prev button and function

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,32 +2,99 @@ import React, { useEffect, useState } from "react";
 import PokeCard from "./PokeCard";
 import axios from "axios";
 import styled from "styled-components";
+import NextButtonImg from "../static/img/next.png";
+import PrevButtonImg from "../static/img/prev.png";
 
 const PokemonList = () => {
-  const url = "https://pokeapi.co/api/v2/pokemon?limit=100";
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=15";
 
   const [pokemons, setPokemons] = useState([]);
+  const [next, setNext] = useState([]);
+  const [prev, setPrev] = useState([]);
 
   useEffect(() => {
     axios.get(url).then((res) => {
       setPokemons(res.data.results);
+      setNext(res.data.next);
+      setPrev(res.data.previous);
     });
   }, []);
 
+  const fetchNewPokemons = (newUrl) => {
+    axios.get(newUrl).then((res) => {
+      setPokemons(res.data.results);
+      setNext(res.data.next);
+      setPrev(res.data.previous);
+    });
+  };
+
   return (
-    <CardContainer>
-      {pokemons.map((pokemon) => (
-        <PokeCard key={pokemon.name} pokemon={pokemon}></PokeCard>
-      ))}
-    </CardContainer>
+    <MainContainer>
+      <PageNav>
+        {prev === null ? (
+          ""
+        ) : (
+          <NavButton
+            backgroundImage={PrevButtonImg}
+            onClick={() => fetchNewPokemons(prev)}
+          ></NavButton>
+        )}
+      </PageNav>
+      <CardContainer>
+        {pokemons.map((pokemon) => (
+          <PokeCard key={pokemon.name} pokemon={pokemon}></PokeCard>
+        ))}
+      </CardContainer>
+      <PageNav>
+        {next === null ? (
+          ""
+        ) : (
+          <NavButton
+            backgroundImage={NextButtonImg}
+            onClick={() => fetchNewPokemons(next)}
+          ></NavButton>
+        )}
+      </PageNav>
+    </MainContainer>
   );
 };
 
-const CardContainer = styled.div`
+const PageNav = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NavButton = styled.button`
+  background-image: url(${(props) => props.backgroundImage}),
+    linear-gradient(#63b8ee, #468ccf);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100px;
+  height: 100px;
+  border: 2px solid #3866a3;
+  color: #14396a;
+  border-radius: 20px;
+  box-shadow: inset 0px 1px 0px 0px #bee2f9;
+  text-shadow: inset 0px 1px 0px #7cacde;
+  :hover {
+    background-image: url(${(props) => props.backgroundImage}),
+      linear-gradient(#468ccf, #63b8ee);
+  }
+`;
+
+const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: 5% 90% 5%;
   grid-area: content;
+  justify-content: center;
+`;
+
+const CardContainer = styled.div`
   display: flex;
   flex: 1;
-  gap: 10px;
+  gap: 5px;
   justify-content: space-around;
   flex-wrap: wrap;
   margin: auto;
